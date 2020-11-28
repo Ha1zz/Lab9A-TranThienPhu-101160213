56961086dc70ff1bacca6e370e721ea6df06b5b7
Ha1zz <earth272272@gmail.com>
2020-11-27 22:21:05 -0500

    Init
    Init

---
+++ Assets/_Scripts/BarController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BarController : MonoBehaviour
+{
+    public Transform bar;
+    public Transform entity;
+    public int currentValue;
+    public int maxValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentValue = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (entity != null)
+        {
+            transform.position = entity.position + Vector3.up + new Vector3(-1.0f,0.0f,0.0f);
+        }
+    }
+
+    public void SetValue(int new_value)
+    {
+        currentValue = new_value;
+        bar.localScale = new Vector3((float)((double)currentValue/(double)maxValue),1.0f,1.0f);
+
+        if (bar.localScale.x < 0)
+        {
+            bar.localScale = new Vector3(0.0f, 1.0f, 1.0f);
+        }    
+    }
+}
